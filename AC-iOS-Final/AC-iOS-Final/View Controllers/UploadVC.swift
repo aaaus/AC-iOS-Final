9d9bfcc3505a58fe55dc9751ebf4d44edb872af3
Melissa He <melissahe@ac.c4q.nyc>
2018-02-26 15:23:18 -0500

    Add keyboard handling

--- AC-iOS-Final/AC-iOS-Final/View Controllers/UploadVC.swift
+++ AC-iOS-Final/AC-iOS-Final/View Controllers/UploadVC.swift
@@ -8,21 +8,77 @@
 
 import UIKit
 import SVProgressHUD
+import NotificationCenter
 
 class UploadVC: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var postTextView: UITextView!
     
+    private var notificationCenter: NotificationCenter!
+    private var keyboardIsShowing: Bool = false
     private var currentUserID: String {
         return AuthUserService.manager.getCurrentUser()!.uid
     }
-    private let databaseService = DatabaseService()
+    private var databaseService: DatabaseService!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        databaseService = DatabaseService()
         databaseService.delegate = self
+        setUpViews()
+        setUpNotification()
         setUpGestures()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    private func setUpViews() {
+        postTextView.layer.masksToBounds = true
+        postTextView.layer.cornerRadius = 10
+        postTextView.layer.borderWidth = 1.0
+        postTextView.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    private func setUpNotification() {
+        notificationCenter = NotificationCenter.default
+        
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: .UIKeyboardWillHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: .UIKeyboardWillShow, object: nil)
+    }
+    
+    @objc private func keyboardWillHide(notification: Notification) {
+        if !keyboardIsShowing {
+            return
+        }
+        print("keyboard will hide")
+        guard let keyboardRect = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect else {
+            print("couldn't get keyboard height")
+            return
+        }
+        let contentInset = scrollView.adjustedContentInset
+        scrollView.contentInsetAdjustmentBehavior = .automatic
+        scrollView.contentInset = UIEdgeInsetsMake(contentInset.top, contentInset.left, contentInset.bottom - keyboardRect.height, contentInset.right)
+        keyboardIsShowing = false
+    }
+    
+    @objc private func keyboardWillShow(notification: Notification) {
+        if keyboardIsShowing {
+            return
+        }
+        print("keyboard will show")
+        guard let keyboardRect = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect else {
+            print("couldn't get keyboard height")
+            return
+        }
+        let contentInset = scrollView.adjustedContentInset
+        scrollView.contentInsetAdjustmentBehavior = .automatic
+        scrollView.contentInset = UIEdgeInsetsMake(contentInset.top, contentInset.left, contentInset.bottom + keyboardRect.height, contentInset.right)
+        scrollView.scrollRectToVisible(postTextView.layer.frame, animated: false)
+        keyboardIsShowing = true
     }
     
     private func setUpGestures() {
@@ -36,10 +92,12 @@ class UploadVC: UIViewController {
     @objc func imagePressed() {
         //to do - set up photo image picker
         print("image pressed!!!")
+        self.view.endEditing(true)
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         //when image set, change the image content mode
+        self.view.endEditing(true)
         print("add button pressed!!")
         guard let image = postImageView.image, image != #imageLiteral(resourceName: "camera_icon") else {
             let errorAlert = Alert.createErrorAlert(withMessage: "Please pick an image to upload!!!", andCompletion: nil)
