20abd0bdaa155b9aeab3d31d88cb20eea1983169
Melissa He <melissahe@ac.c4q.nyc>
2018-02-26 12:37:57 -0500

    Add StorageService file

--- AC-iOS-Final/AC-iOS-Final/Models/Services/StorageService.swift
+++ AC-iOS-Final/AC-iOS-Final/Models/Services/StorageService.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2018 C4Q . All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Toucan
+import FirebaseStorage
+
+class StorageService {
+    init() {
+        self.ref = Storage.storage().reference()
+        self.imageRef = self.ref.child("images")
+    }
+    private let ref: StorageReference!
+    private let imageRef: StorageReference!
+    
+    public func storeImage(withPostID postID: String, andImage image: UIImage, completion: @escaping (URL?, ErrorMessage?) -> Void) {
+        //resize image
+        guard let toucanImage = Toucan(image: image).resize(CGSize(width: 500, height: 500)).image else {
+            print("could not resize image")
+            completion(nil, "could not resize image")
+            return
+        }
+        
+        //convert image to png data
+        guard let imageData = UIImagePNGRepresentation(toucanImage) else {
+            print("couldn't get png image data")
+            completion(nil, "couldn't get png image data")
+            return
+        }
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/png"
+        
+        imageRef.child(postID).putData(imageData, metadata: metadata) { (metadata, error) in
+            if let error = error {
+                print("could not store image: \(error.localizedDescription)")
+                completion(nil, "could not store image: \(error.localizedDescription)")
+            } else if let metadata = metadata {
+                let imageDownloadURL = metadata.downloadURL()
+                completion(imageDownloadURL, nil)
+            }
+        }
+    }
+}
