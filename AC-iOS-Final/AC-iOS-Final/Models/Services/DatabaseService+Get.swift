6ea0d4eb95b40822c86a16a28098b3d721601eee
Melissa He <melissahe@ac.c4q.nyc>
2018-02-26 11:42:02 -0500

    Add DatabaseService Get file

--- AC-iOS-Final/AC-iOS-Final/Models/Services/DatabaseService+Get.swift
+++ AC-iOS-Final/AC-iOS-Final/Models/Services/DatabaseService+Get.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+extension DatabaseService {
+    public func getPosts(completion: @escaping ([Post]?) -> Void) {
+        postsRef.observe(.value) { (dataSnapshot) in
+            guard let postsSnapshot = dataSnapshot.children.allObjects as? [DataSnapshot] else {
+                print("Error: Could not get children datasnapshots.")
+                self.delegate?.didFailGettingPosts?(errorMessage: "Could not get children datasnapshots.")
+                return
+            }
+            var posts: [Post] = []
+            for postSnapshot in postsSnapshot {
+                guard let postDict = postSnapshot.value as? [String : Any] else {
+                    print("Error: Could not initialize postDict from postSnapshot.")
+                    self.delegate?.didFailGettingPosts?(errorMessage: "Could not initialize postDict from postSnapshot.")
+                    return
+                }
+                guard let post = Post(from: postDict) else {
+                    print("Error: Could not instantiate post from postDict.")
+                    self.delegate?.didFailGettingPosts?(errorMessage: "Could not instantiate post from postDict.")
+                    return
+                }
+                posts.append(post)
+            }
+            completion(posts)
+        }
+    }
+}
